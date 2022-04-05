59f02d147a3be46e3f0933642b586def103e4a1f
keeprunning <87924127+keeprunni@users.noreply.github.com>
2022-04-05 11:18:26 +0800

    Add files via upload

--- DataStructure/Search/BinarySortTree.cpp
+++ DataStructure/Search/BinarySortTree.cpp
@@ -24,6 +24,8 @@ typedef struct
 void Insert(Tree pT, int val);
 void CreatBTree(Tree pHead);
 void TraveseTree(PBTNODE root);
+void DeleteBTNode(PBTNODE root, int val);
+int FindMaxBTNode(PBTNODE lchild);
 
 int main(void)
 {
@@ -31,6 +33,11 @@ int main(void)
     pT.root = NULL;     //root必须指向NULL，否则在Insert()内会访问root指向的地址
 
     CreatBTree(&pT);
+    TraveseTree(pT.root);
+
+    int val = 45;
+    printf("删除结点值为%d后, 二叉树的先序值: \n", val);
+    DeleteBTNode(pT.root, val);
     TraveseTree(pT.root);
     return 0;
 }
@@ -109,4 +116,59 @@ void TraveseTree(PBTNODE root)
 
     if(ptr->rchild != NULL)
         TraveseTree(ptr->rchild);
+}
+
+void DeleteBTNode(PBTNODE root, int val)
+{
+    PBTNODE ptr = root;
+
+    while( ptr != NULL)
+    {
+        if(ptr->data.value == val) break; 
+
+        if(ptr->data.value > val) ptr = ptr->lchild;
+        else ptr = ptr->rchild;
+    }
+
+    if(ptr == NULL)
+    {
+        printf("您所删除的值在二叉树中不存在！\n");
+        return;
+    }
+
+    if((ptr->lchild != NULL) && (ptr->rchild != NULL))
+    {
+        int q;
+        q = FindMaxBTNode(ptr->lchild);     //找出左子树的最大值或右子树的最小值
+        ptr->data.value = q;            //替换当前结点的值
+        DeleteBTNode(ptr->lchild, q);   //找出左子树最大值的结点或右子树最小值的结点，并删除
+    }
+    else if( (ptr->lchild != NULL) && (ptr->rchild == NULL))   //左子树非空
+    {
+        PBTNODE q = ptr->lchild;
+        ptr->data.value = q->data.value;
+        ptr->lchild = NULL;
+        free(q);
+    }
+    else if((ptr->rchild != NULL) && (ptr->lchild == NULL))    //右子树非空
+    {
+        PBTNODE q = ptr->rchild;
+        ptr->data.value = q->data.value;
+        ptr->rchild = NULL;
+        free(q);
+    }
+    else 
+        free(ptr);
+}
+
+int FindMaxBTNode(PBTNODE lchild)
+{
+    if(lchild == NULL)
+        return 0;
+    else if(lchild->rchild == NULL)
+        return lchild->data.value;
+    else if(lchild->rchild != NULL)
+        FindMaxBTNode(lchild->rchild);
+    
+    return 0;
 }
